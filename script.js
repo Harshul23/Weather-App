35aa785ae77f92c4ac0fe44d557989958f150465
Harshul <vishuseervi825@gmail.com>
2026-01-30 11:13:50 +0530

    Created a Weather App

---
+++ script.js
@@ -0,0 +1,49 @@
+const searchBtn = document.querySelector(".Search-btn")
+const input = document.querySelector(".location-input")
+const apiKey = "d2ddb2b4e596e23d9ef52069cecf30aa";
+
+// Listen enter key down event
+input.addEventListener("keydown", (event) => {
+  if (event.key == "Enter") {
+    if (event.key === "Enter" && input.value) getWeatherData(input.value);
+    // update DOM
+    let location = document.querySelector(".Location")
+    // log the input Location in console
+    console.log(input.value)
+    location.innerHTML = input.value.trim()
+    //clear input
+    input.value = ""
+  }
+})
+
+// Listen click event
+searchBtn.addEventListener("click", () => {
+  if (input.value) getWeatherData(input.value);
+  console.log(input.value)
+  input.value = ""
+})
+
+
+async function getWeatherData(city) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    );
+    
+    if (!response.ok) throw new Error("City not found or API key invalid");
+
+    const data = await response.json();
+    updateUI(data);
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+
+function updateUI(data) {
+  document.querySelector(".Temprature").innerText = `${Math.round(data.main.temp)}°C`;
+  document.querySelector(".condition").innerText = data.weather[0].main;
+  document.querySelector(".Location").innerText = data.name;
+  const iconCode = data.weather[0].icon;
+  document.querySelector(".icon img").src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+}
