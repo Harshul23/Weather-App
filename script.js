b0e8a6df463a1acd380a978e43c595a892a426df
Harshul <vishuseervi825@gmail.com>
2026-02-03 13:11:04 +0530

    UI improvements

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
-const searchBtn = document.querySelector(".Search-btn")
-const input = document.querySelector(".location-input")
-const apiKey = "d2ddb2b4e596e23d9ef52069cecf30aa";
+import { API_KEY } from "./config.js";
+const searchBtn = document.querySelector(".Search-btn");
+const input = document.querySelector(".location-input");
+const apiKey = API_KEY;
+
+console.log("API Key:", apiKey);
 
 // Listen enter key down event
 input.addEventListener("keydown", (event) => {
   if (event.key == "Enter") {
     if (event.key === "Enter" && input.value) getWeatherData(input.value);
-    // update DOM
-    let location = document.querySelector(".Location")
     // log the input Location in console
-    console.log(input.value)
-    location.innerHTML = input.value.trim()
+    console.log(input.value);
     //clear input
-    input.value = ""
+    input.value = "";
   }
-})
+});
 
 // Listen click event
 searchBtn.addEventListener("click", () => {
   if (input.value) getWeatherData(input.value);
-  console.log(input.value)
-  input.value = ""
-})
+  console.log(input.value);
+  input.value = "";
+});
 
 // Fetch weather data from OpenWeatherMap API
 async function getWeatherData(city) {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`,
     );
-    
-    if (!response.ok) throw new Error("City not found or API key invalid");
-
+    // Catch the invalid input or not found error
+    if (!response.ok) throw new Error("City not found");
     const data = await response.json();
-    updateUI(data);
+    updateDOM(data);
+
+    // Calling function to Update UI
   } catch (error) {
     alert(error.message);
   }
 }
 
 // Update the UI with fetched weather data
-function updateUI(data) {
-  document.querySelector(".Temprature").innerText = `${Math.round(data.main.temp)}°C`;
+function updateDOM(data) {
+  const temprature = data.main.temp;
+  const location = data.name;
+  console.log(data);
+  // Temprature condition and location.
+  document.querySelector(".Temprature").innerText =
+    `${Math.round(temprature)}°C`;
   document.querySelector(".condition").innerText = data.weather[0].main;
-  document.querySelector(".Location").innerText = data.name;
+  document.querySelector(".Location").innerText = location;
   const iconCode = data.weather[0].icon;
-  document.querySelector(".time").innerText = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-  document.querySelector(".day").innerText = new Date().toLocaleDateString([], { weekday: 'long' });
+  // Date, Time, month, Year and Day
+  document.querySelector(".time").innerText = new Date().toLocaleTimeString(
+    [],
+    { hour: "2-digit", minute: "2-digit" },
+  );
+  document.querySelector(".day").innerText = new Date().toLocaleDateString([], {
+    weekday: "short",
+  });
   document.querySelector(".date").innerText = new Date().getDate();
-  document.querySelector(".month").innerText = new Date().toLocaleDateString([], { month: 'long' });
+  document.querySelector(".month").innerText = new Date().toLocaleDateString(
+    [],
+    { month: "long" },
+  );
   document.querySelector(".year").innerText = new Date().getFullYear();
-  document.querySelector(".icon img").src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  // Weather Status Icon
+  document.querySelector(".icon img").src =
+    `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 }
